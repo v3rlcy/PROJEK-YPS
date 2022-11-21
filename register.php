eed8fc73d0c27a3122b0adbaa034af722ad7de10
v3rlcy <verlacya@gmail.com>
2022-11-21 21:43:34 +0700

    apa

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+
+<h1>Sign Up</h1>
+<p>Sign Up to have more experience with us!</p>
+
+<form action="" method="post">
+    <ul>
+        <li>
+            <label for="username">Username</label>
+            <input type="text" name="username">
+        </li>
+        <li>
+            <label for="password">Your Password</label>
+            <input type="password" name="password">
+        </li>
+        <li>
+            <button type="submit" name="login">Sign Up Now!</button>
+        </li>
+    </ul>
+</form>
+
+</body>
+</html>
