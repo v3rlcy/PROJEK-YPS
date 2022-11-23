860cec4cf37f8baf279e6ebb0052c72313b749d3
JovBntro <jovanbuntoro66@gmail.com>
2022-11-23 08:39:26 +0700

    login register bootstrap

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <a class="navbar-brand" href="#">Rumah Sakit</a>
+      <a class="navbar-brand" href="#">Rumah Sakit YPS</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <a class="nav-link" aria-current="page" href="index.php">Home</a>
-          <a class="nav-link" href="register.php">Sign Up</a>
+          <a class="nav-link" href="register.php">Register</a>
           <a class="nav-link" href="login.php">Login</a>
         </div>
       </div>
--- register.php
+++ register.php
@@ -23,38 +23,49 @@ if (isset($_POST["register"])) {
   <title>Sign Up</title>
   <link rel="stylesheet" href="style.css">
 </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
 
 <body>
 
-  <h1>Sign Up</h1>
-  <p>Sign Up to have more experience with us!</p>
-  <form action="" method="post">
-    <ul>
-      <li>
-        <label for="nama">Nama</label>
-        <input type="text" name="nama_user">
-      </li>
-      <li>
-        <label for="username">Username</label>
-        <input type="text" name="username">
-      </li>
-      <li>
-        <label for="password">Your password</label>
-        <input type="password" name="password">
-      </li>
-      <li>
-        <label for="password">Confirm your password</label>
-        <input type="password" name="password2">
-      </li>
-      <li>
-        <button type="submit" name="register">Sign Up Now!</button>
-      </li>
-    </ul>
+  <div class="container pt-3 mt-5">
+    <h1 style="text-align:center;" >YPS Hospital</h1>
+  <form>
+  <!-- Email input -->
+  <div class="form-outline mb-4">
+    <label class="form-label" for="nama">Nama</label>
+    <input type="text" name="nama_user" class="form-control" />
+  </div>
 
-    Already have an account?<a href="login.php">Login</a>
+  <!-- Password input -->
+  <div class="form-outline mb-4">
+    <label class="form-label" for="username">Username</label>
+    <input type="text" name="username" class="form-control" />
+  </div>
+  
+  <div class="form-outline mb-4">
+    <label class="form-label" for="password">Password</label>
+    <input type="password" name="password" class="form-control" />
+  </div>
 
-  </form>
+  <div class="form-outline mb-4">
+    <label class="form-label" for="password">Confirm Password</label>
+    <input type="password" name="password2" class="form-control" />
+  </div>
 
+  <!-- Submit button -->
+  <div class="mb-4">
+  <button type="button" class="btn btn-success btn-block mb-4 w-100 " name="register">Sign in</button>
+  </div>
+
+  <p>Already have an account? <a href="login.php">Login </a></p>
+
+</form>
+</div>
 </body>
 
 </html>
+
+
+
