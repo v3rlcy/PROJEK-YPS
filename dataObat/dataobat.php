1064b5f6829de6cdb87b9612f2d5862662dc18c5
JovBntro <jovanbuntoro66@gmail.com>
2022-11-23 22:25:53 +0700

    bootstrap kesekian

--- dataObat/dataobat.php
+++ dataObat/dataobat.php
@@ -1,5 +1,5 @@
 <?php
-require './function.php';
+require '..\function.php';
 $obat = query("SELECT * FROM tb_obat");
 
 if (isset($_POST["cari"])) {
@@ -16,57 +16,112 @@ if (isset($_POST["cari"])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 
-
-  <h1 class="text-center">LIST OBAT</h1>
-
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<style> 
+body{
+  background-color: #d9d9d9;
+}
+
+.wrapper {
+  display: flex;
+  width: 100%;
+  align-items: stretch;
+}
+
+</style>
 
 <body>
 
+<div class="wrapper">
+  <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-light" style="height: 1080px; width: 280px;">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-4 text-dark fw-bold">YPS Hospital</span>
+      </a>
+      <hr>
+      
+
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+        <li>
+          <a href="..\index.php" class="nav-link text-dark">Dashboard</a>
+        </li>
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Data Dokter
+          </a>
+        </li>
+
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Data Pasien
+          </a>
+        </li>
+        
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Poli Klinik
+          </a>
+        </li>
+
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Rekam Medis
+          </a>
+        </li>
+
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link active text-light aria-current="page"">Data Obat
+          </a>
+        </li>  
+
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Rm obat ni tulis ape cok
+          </a>
+        </li>  
+
+      </ul>
+      <hr>
+    </div>
+
   <div class="container">
-
-    <div>
-      <form action="" method="post">
-        <input type="text" name="keyword" size=40 placeholder="Search..." autocomplete="off" class="outline-dark">
-        <button type="submit" name="cari" class="btn btn-primary">Search</button>
-      </form>
+    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4 fw-normal">YPS Hospital</h1>
+        <p class="fs-5 text-muted">Data Obat</p>
     </div>
 
-    <div class="text-end">
-      <button class="btn btn-outline-info">
-        <a href="tambahobat.php">TAMBAHKAN DATA</a>
-      </button>
-    </div>
+      <div class="form-group">
+          <form action="" method="post" >
+              <div class="row">
+                  <div class="input-group mb-2">
+                      <input type="text" name="keyword" class="form-control" autofocus placeholder="Masukkan keyword pencaharian" autocomplete="off" id="keyword">
+                      <button class="btn btn-info" type="submit" name="cari" id="tombol-cari">Cari!</button>
+                  </div>
+          </form>
+      </div>
+      <a class="btn btn-primary" href="tambah.php">Tambah data siswa</a>
 
-  </div>
+  
+      <table class="table table-bordered border-primary text-center mt-2">
+          <tr class="table-success">
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Nama Obat</th>
+            <th>Keterangan Obat</th>
+          </tr>
 
-  <table border=1 cellpadding=20 cellspacing=0 class="table table-bordered">
-    <tr class="table-primary">
-      <th>No.</th>
-      <th>Aksi</th>
-      <th>Nama Obat</th>
-      <th>Keterangan Obat</th>
-    </tr>
-
-    <?php $i = 1;
-    foreach ($obat as $row) : ?>
-      <tr>
-        <td><?= $row['id_obat']; ?></td>
-        <td>
-          <button class="btn btn-outline-danger">
-            <a href="hapusobat.php?id_obat=<?= $row['id_obat'] ?>">HAPUS</a>
-          </button>
-          <button class="btn btn-outline-info">
-            <a href="ubahobat.php?id_obat=<?= $row['id_obat'] ?>">UBAH</a>
-          </button>
-        </td>
-        <td><?= $row['nama_obat']; ?></td>
-        <td><?= $row['ket_obat']; ?></td>
-      <?php endforeach; ?>
-      </tr>
-  </table>
-
+        <?php $i = 1;
+        foreach ($obat as $row) : ?>
+          <tr>
+            <td><?= $row['id_obat']; ?></td>
+            <td>
+                <a class="btn btn-danger" href="hapusobat.php?id_obat=<?= $row['id_obat'] ?>">HAPUS</a>
+                <a class="btn btn-info" href="ubahobat.php?id_obat=<?= $row['id_obat'] ?>">UBAH</a>
+            </td>
+            <td><?= $row['nama_obat']; ?></td>
+            <td><?= $row['ket_obat']; ?></td>
+          <?php endforeach; ?>
+          </tr>
+        </table>
+  </div> 
+</div>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -9,105 +9,170 @@
 </head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<style>
+body{
+  background-color: #d9d9d9;
+}
 
+.wrapper {
+  display: flex;
+  width: 100%;
+  align-items: stretch;
+}
+  
+</style>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <h2 class="navbar-brand" href="#">YPS Hospital</h2>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link" aria-current="page" href="index.php">Home</a>
-          <a class="nav-link" href="register.php">Register</a>
-          <a class="nav-link" href="login.php">Login</a>
+
+<div class="wrapper">
+  <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-light" style="height: 1080px; width: 280px;">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-4 text-dark fw-bold">YPS Hospital</span>
+      </a>
+      <hr>
+      
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+        <li>
+          <a href="#" class="nav-link active text-light aria-current="page">Dashboard</a>
+        </li>
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Data Dokter
+          </a>
+        </li>
+
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Data Pasien
+          </a>
+        </li>
+        
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Poli Klinik
+          </a>
+        </li>
+
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Rekam Medis
+          </a>
+        </li>
+
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Data Obat
+          </a>
+        </li>  
+
+        <li>
+          <a href="dataObat/dataobat.php" class="nav-link text-dark">Rm obat ni tulis ape cok
+          </a>
+        </li>  
+
+      </ul>
+      <hr>
+    </div>
+
+    
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <h2 class="navbar-brand" href="#">YPS Hospital</h2>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link" href="dashboard.php">Dashboard</a>
+            <a class="nav-link" href="register.php">Register</a>
+            <a class="nav-link" href="login.php">Login</a>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
-  <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-      <h1 class="display-4 fw-normal">YPS Hospital</h1>
-      <p class="fs-5 text-muted">"Kepala sakit namun PHP tetaplah asik 🔥👍"</p>
+    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4 fw-normal">YPS Hospital</h1>
+        <p class="fs-5 text-muted">"Kepala sakit namun PHP tetaplah asik 🔥👍"</p>
     </div>
 
 <!-- crudnya -->
-<div class="container pt-3 mt-5" >
-  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-        <div class="col">
-          <div class="card mb-4 rounded-3 shadow-sm border-success">
-            <div class="card-header py-3 text-bg-success border-success">
-              <h4 class="my-0 fw-normal">Data Dokter</h4>
+    <div class="container">
+      <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm border-success">
+                <div class="card-header py-3 text-bg-success border-success">
+                  <h4 class="my-0 fw-normal">Data Dokter</h4>
+                </div>
+                <div class="card-body">
+                <p>Berisikan data - data dokter di YPS Hospital</p>
+                  <a class="w-100 btn btn-primary" href="dokter/datadokter.php">Masuk</a>
+                </div>
+              </div>
             </div>
-            <div class="card-body">
-            <p>Berisikan data - data dokter di YPS Hospital</p>
-              <a class="w-100 btn btn-primary" href="dokter/datadokter.php">Masuk</a>
-            </div>
-          </div>
-        </div>
 
-        <div class="col">
-          <div class="card mb-4 rounded-3 shadow-sm border-primary">
-            <div class="card-header py-3 text-bg-primary border-primary">
-              <h4 class="my-0 fw-normal">Data Pasien</h4>
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm border-info">
+                <div class="card-header py-3 text-bg-info border-info">
+                  <h4 class="my-0 fw-normal text-light">Data Pasien</h4>
+                </div>
+                <div class="card-body">
+                <p>Berisi data - data tentang pasien yang dirawat di YPS Hospital</p>
+                  <a class="w-100 btn btn-primary" href="pasien/datapasien.php">Masuk</a>
+                </div>
+              </div>
             </div>
-            <div class="card-body">
-            <p>Berisi data - data tentang pasien yang dirawat di YPS Hospital</p>
-              <a class="w-100 btn btn-primary" href="pasien/datapasien.php">Masuk</a>
-            </div>
-          </div>
-        </div>
 
-        <div class="col">
-          <div class="card mb-4 rounded-3 shadow-sm border-primary">
-            <div class="card-header py-3 text-bg-primary border-primary">
-              <h4 class="my-0 fw-normal">PoliKlinik</h4>
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm border-warning">
+                <div class="card-header py-3 text-bg-warning border-warning">
+                  <h4 class="my-0 fw-normal text-light">PoliKlinik</h4>
+                </div>
+                <div class="card-body">
+                <p>Anda dapat melihat informasi mengenai Poliklinik yang ada</p>
+                  <a class="w-100 btn btn-primary" href="poliklinik/datapoli.php">Masuk</a>
+                </div>
+              </div>
             </div>
-            <div class="card-body">
-            <p>Anda dapat melihat informasi mengenai Poliklinik yang ada</p>
-              <a class="w-100 btn btn-primary" href="poliklinik/datapoli.php">Masuk</a>
-            </div>
-          </div>
-        </div>
 
-        <div class="col">
-          <div class="card mb-4 rounded-3 shadow-sm border-primary">
-            <div class="card-header py-3 text-bg-primary border-primary">
-              <h4 class="my-0 fw-normal">Rekam Medis</h4>
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm border-danger">
+                <div class="card-header py-3 text-bg-danger border-danger">
+                  <h4 class="my-0 fw-normal">Rekam Medis</h4>
+                </div>
+                <div class="card-body">
+                <p>Anda dapat melihat riwayat atau rekam medis pasien</p>
+                  <a class="w-100 btn btn-primary" href="rekammedis/datarekammedis.php">Masuk</a>
+                </div>
+              </div>
             </div>
-            <div class="card-body">
-            <p>Anda dapat melihat riwayat atau rekam medis pasien</p>
-              <a class="w-100 btn btn-primary" href="rekammedis/datarekammedis.php">Masuk</a>
-            </div>
-          </div>
-        </div>
 
-        <div class="col">
-          <div class="card mb-4 rounded-3 shadow-sm border-primary">
-            <div class="card-header py-3 text-bg-primary border-primary">
-              <h4 class="my-0 fw-normal">Data Obat</h4>
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm border-primary">
+                <div class="card-header py-3 text-bg-primary border-primary">
+                  <h4 class="my-0 fw-normal">Data Obat</h4>
+                </div>
+                <div class="card-body">
+                <p>Berisikan data mengenai obat yang ada</p>
+                  <a class="w-100 btn btn-primary" href="dataObat/dataobat.php">Masuk</a>
+                </div>
+              </div>
             </div>
-            <div class="card-body">
-            <p>Berisikan data mengenai obat yang ada</p>
-              <a class="w-100 btn btn-primary" href="dataObat/dataobat.php">Masuk</a>
-            </div>
-          </div>
-        </div>
 
-        <div class="col">
-          <div class="card mb-4 rounded-3 shadow-sm border-primary">
-            <div class="card-header py-3 text-bg-primary border-primary">
-              <h4 class="my-0 fw-normal">Rm obat ni tulis ape cok</h4>
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm border-primary">
+                <div class="card-header py-3 text-bg-primary border-primary">
+                  <h4 class="my-0 fw-normal">Rm obat ni tulis ape cok</h4>
+                </div>
+                <div class="card-body">
+                <p>Bantu pikir rm obat cok</p>
+                  <a class="w-100 btn btn-primary" href="dataObat/dataobat.php">Masuk</a>
+                </div>
+              </div>
             </div>
-            <div class="card-body">
-            <p>Bantu pikir rm obat cok</p>
-              <a class="w-100 btn btn-primary" href="dataObat/dataobat.php">Masuk</a>
-            </div>
-          </div>
-        </div>
-  </div>
-</div> 
+      </div>
+      </div>
+    </div>
+</div>
 
 </body>
 
