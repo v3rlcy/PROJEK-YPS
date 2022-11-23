35ad1b8a63214d65d311b2e6e2077f2b5eb6af9c
myoshuu <myoshu.me@gmail.com>
2022-11-23 12:28:46 +0700

    fix folder name & file position

--- auth/login.php
+++ auth/login.php
@@ -12,7 +12,7 @@ if (isset($_POST['login'])) {
   if (mysqli_num_rows($res) === 1) {
     $row = mysqli_fetch_assoc($res);
     if (password_verify($password, $row["password"])) {
-      header("Location: index.php");
+      header("Location: ../index.php");
     }
   }
 
@@ -36,9 +36,7 @@ if (isset($_POST['login'])) {
 
   <!-- <h1>Login</h1>
   <p>Login to have more experience with us!</p>
-  <?php if (isset($error)) : ?>
     <p class="alert danger">Username / Password Salah</p>
-  <?php endif; ?>
   <form action="" method="post">
     <ul>
       <li>
@@ -61,27 +59,28 @@ if (isset($_POST['login'])) {
   <?php endif; ?>
 
   <div class="container pt-3 mt-5">
-    <h1 style="text-align:center;" >YPS Hospital</h1>
-  
+    <h1 style="text-align:center;">YPS Hospital</h1>
+
     <form action="" method="post">
 
-  <div class="form-outline mb-4">
-    <label class="form-label" for="nama">Username</label>
-    <input type="text" name="username" class="form-control" />
-  </div>
+      <div class="form-outline mb-4">
+        <label class="form-label" for="nama">Username</label>
+        <input type="text" name="username" class="form-control" />
+      </div>
 
-  <div class="form-outline mb-4">
-    <label class="form-label" for="nama">Password</label>
-    <input type="password" name="password" class="form-control" />
-  </div>
+      <div class="form-outline mb-4">
+        <label class="form-label" for="nama">Password</label>
+        <input type="password" name="password" class="form-control" />
+      </div>
 
-  <!-- login button -->
-  <div class="mb-4">
-  <button type="submit" class="btn btn-success btn-block mb-4 w-100 " name="login">Log in</button>
-  </div>
+      <!-- login button -->
+      <div class="mb-4">
+        <button type="submit" class="btn btn-success btn-block mb-4 w-100 " name="login">Log in</button>
+      </div>
 
-  <p>Don't have an account? <a href="register.php">Sign Up now! </a></p>
-  </form>
+      <p>Don't have an account? <a href="register.php">Sign Up now! </a></p>
+    </form>
 
 </body>
+
 </html>
