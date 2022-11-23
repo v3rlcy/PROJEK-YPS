05de2b633db91c18f6e0f1b06ab52f4633f5ca50
JovBntro <jovanbuntoro66@gmail.com>
2022-11-23 20:22:51 +0700

    Bootstrap dashboard 1

--- index.php
+++ index.php
@@ -26,14 +26,88 @@
       </div>
     </div>
   </nav>
-  <div class="mt-4 container">
-    <h3 class="fw-semibold">Project Tercinta</h3>
-    <p>Team YPS</p>
-    <div class="d-flex gap-3 align-items-center">
-      <a class="text-black text-decoration-none" href="login.php">Login</a>
-      <a class="btn btn-primary" href="register.php">Register</a>
+
+  <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+      <h1 class="display-4 fw-normal">YPS Hospital</h1>
+      <p class="fs-5 text-muted">"Kepala sakit namun PHP tetaplah asik 🔥👍"</p>
     </div>
-  </div> -->
+
+<!-- crudnya -->
+<div class="container pt-3 mt-5" >
+  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm border-success">
+            <div class="card-header py-3 text-bg-success border-success">
+              <h4 class="my-0 fw-normal">Data Dokter</h4>
+            </div>
+            <div class="card-body">
+            <p>Berisikan data - data dokter di YPS Hospital</p>
+              <a class="w-100 btn btn-primary" href="dokter/datadokter.php">Masuk</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm border-primary">
+            <div class="card-header py-3 text-bg-primary border-primary">
+              <h4 class="my-0 fw-normal">Data Pasien</h4>
+            </div>
+            <div class="card-body">
+            <p>Berisi data - data tentang pasien yang dirawat di YPS Hospital</p>
+              <a class="w-100 btn btn-primary" href="pasien/datapasien.php">Masuk</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm border-primary">
+            <div class="card-header py-3 text-bg-primary border-primary">
+              <h4 class="my-0 fw-normal">PoliKlinik</h4>
+            </div>
+            <div class="card-body">
+            <p>Anda dapat melihat informasi mengenai Poliklinik yang ada</p>
+              <a class="w-100 btn btn-primary" href="poliklinik/datapoli.php">Masuk</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm border-primary">
+            <div class="card-header py-3 text-bg-primary border-primary">
+              <h4 class="my-0 fw-normal">Rekam Medis</h4>
+            </div>
+            <div class="card-body">
+            <p>Anda dapat melihat riwayat atau rekam medis pasien</p>
+              <a class="w-100 btn btn-primary" href="rekammedis/datarekammedis.php">Masuk</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm border-primary">
+            <div class="card-header py-3 text-bg-primary border-primary">
+              <h4 class="my-0 fw-normal">Data Obat</h4>
+            </div>
+            <div class="card-body">
+            <p>Berisikan data mengenai obat yang ada</p>
+              <a class="w-100 btn btn-primary" href="dataObat/dataobat.php">Masuk</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm border-primary">
+            <div class="card-header py-3 text-bg-primary border-primary">
+              <h4 class="my-0 fw-normal">Rm obat ni tulis ape cok</h4>
+            </div>
+            <div class="card-body">
+            <p>Bantu pikir rm obat cok</p>
+              <a class="w-100 btn btn-primary" href="dataObat/dataobat.php">Masuk</a>
+            </div>
+          </div>
+        </div>
+  </div>
+</div> 
 
 </body>
 
